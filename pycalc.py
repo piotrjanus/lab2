ab3102599b3f43fb6cc421be5ac0f4098b54b9d9
piotrjanus <piotrjanusjanus@gmail.com>
2017-03-20 19:55:02 +0100

    adding exceptions and basic unit test

--- pycalc.py
+++ pycalc.py
@@ -1,4 +1,13 @@
 import abc
+from math import log
+
+class notIntNotFloat(Exception):
+	def __str__(self):
+		return repr("not int nor float")
+
+class divisionByZero(Exception):
+	def __str__(self):
+		return repr("division by zero!")
 
 class abstractCalc(object):
 	__metaclass__  = abc.ABCMeta
@@ -17,11 +26,31 @@ class abstractCalc(object):
 #add derivative
 
 class calc(abstractCalc):
+
 	def add(self, x, y):
+		self.isIntOrFloat(x)
+		self.isIntOrFloat(y)
 		return x+y
 
 	def divide(self, x, y):
+		self.isIntOrFloat(x)
+		self.isIntOrFloat(y)
 		return x/y
 	
-	def ln(self, x, y):
-		pass
+	def ln(self, x):
+		return math.log(x)
+
+	def isZero(self, x):
+		if x == 0:
+			raise divisionByZero()
+	
+	def isIntOrFloat(self, x):
+		if not isinstance(x,(int, float)):
+			raise notIntNotFloat()
+
+
+
+
+
+
+
--- pycalc_test.py
+++ pycalc_test.py
@@ -1,11 +1,16 @@
 import unittest
 from pycalc import calc
+from pycalc import notIntNotFloat
 
 
 class test_calc(unittest.TestCase):
-	def test_adding_int_string(self):
+
+	def test_adding_int_and_string(self):
 		c = calc()
-		self.assertEqual(c.add(2,2),4)
+		self.assertRaises(notIntNotFloat,c.add,"sd",2)
+	
+	def test_division_by_zero(self):
+		pass
 
 if __name__ == '__main__':
 	unittest.main()
